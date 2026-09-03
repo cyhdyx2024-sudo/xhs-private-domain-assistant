c260ca0a2822561e0ed4a6039ead1628a85dd195
cyhdyx2024-sudo <cyhdyx2024@gmail.com>
2026-09-03 12:48:43 +0800

    chore: remove internal audit/evaluation reports and update .gitignore

--- server/clean_old_test_data.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("server/data/xhs_reply_feedback.sqlite3")
-conn.execute("DELETE FROM reply_feedback")
-conn.commit()
-print("Cleaned reply_feedback table")
--- server/verify_api.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json
-token = 'xhs_live_87EBDO26zZKNv7NlmuZgc-TOaSyRyH7UU47EPMH-Tag'
-req = urllib.request.Request('http://127.0.0.1:18195/knowledge/documents', headers={'Authorization': f'Bearer {token}'})
-with urllib.request.urlopen(req) as resp:
-    print('Docs Result:', json.loads(resp.read().decode('utf-8')))
-req2 = urllib.request.Request('http://127.0.0.1:18195/knowledge/faq/list', headers={'Authorization': f'Bearer {token}'})
-with urllib.request.urlopen(req2) as resp:
-    print('FAQ Result:', json.loads(resp.read().decode('utf-8')))
